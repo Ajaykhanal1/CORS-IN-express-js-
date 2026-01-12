3137f52d309be533a8642d737685577d13a02f2f
Ajay Khanal <khanalajay9848@gmail.com>
2026-01-12 18:18:38 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
+app.get('/',(req,res)=>{
+    res.send("CORS enabled for this frontend porn in 3000.");
+});
+
+app.listen(3000);
+
+//      Only this frontend can access backend
